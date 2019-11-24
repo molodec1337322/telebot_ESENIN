dd751d39c72c631346843c107c45a2edaec9d01a
Alexander Shibanov <test_no_reply@gmail.com>
2019-11-24 13:48:19 +0300

    0,9

--- bot.py
+++ bot.py
@@ -8,31 +8,49 @@ bot_token = "808248861:AAGOwZ8zaxh6qOO0t9h-N8JRF3yMilh2ioA"
 def get_tags(tag_list, number_of_tags, database):
     """ вбивает теги """
 
-    for i in range(number_of_statements):
+    for i in range(number_of_tags):
         if database['quotes'][i]['tag'] not in tag_list:
-            tag_list.append(statement_database['quotes'][i]['tag']);
+            tag_list.append(database['quotes'][i]['tag']);
+
+
+def tag_data(dict_to_tag, number_of_tags, database):
+    """ распределяет текст по тегам в словаре """
+
+    data = database['quotes']
+    for i in range(len(data)):
+        if data[i]['tag'] not in dict_to_tag:
+            temp_dict = {data[i]['tag'] : [data[i]['text']]}
+            dict_to_tag.update(temp_dict)
+
+        else:
+            print(data[i]['tag'])
+            temp_text_list = dict_to_tag[data[i]['tag']]['text']
+            temp_text_list.append(data[i]['text'])
+            dict_to_tag[data[i]['tag']][temp_text_list]
+
+    print(dict_to_tag)
 
 
 # цитаты
-statements_database_filename = 'statements_database.json'
-statement_database = deserialize.read_data(statements_database_filename)
-number_of_statements = len(statement_database['quotes'])
-avaliable_statement_tags = []
-get_tags(avaliable_statement_tags, number_of_statements, statement_database)
+quotes_database_filename = 'quotes_database.json'
+quotes_database = deserialize.read_data(quotes_database_filename)
+number_of_quotes = len(quotes_database['quotes'])
+avaliable_quotes_tags = []
+tagged_quotes = {}
+get_tags(avaliable_quotes_tags, number_of_quotes, quotes_database)
+#tag_data(tagged_quotes, number_of_quotes, quotes_database)
 
 
 # стихи
 poems_database_filename = 'poems_database.json'
 poems_database = deserialize.read_data(poems_database_filename)
-number_of_poems = len(poems_database['quotes'])
-avaliable_poem_tags = []
-get_tags(avaliable_poem_tags, number_of_poems, poems_database)
+number_of_poems = len(poems_database['poems'])
 
 
 # биография
 boigraphy_database_filename = 'biography_database.json'
 biography_database = deserialize.read_data(boigraphy_database_filename)
-length_of_boigraphy = len(poems_database['quotes'])
+length_of_boigraphy = len(biography_database['facts'])
 
 bot = tb.TeleBot(bot_token)
 
@@ -40,17 +58,23 @@ bot = tb.TeleBot(bot_token)
 keyboard_commands_root = tb.types.ReplyKeyboardMarkup(True)
 keyboard_commands_root.row("Цитата", "Стих", "Биография")
 
-# кейборд для всплывающих тегов
-tags_keyboard_panel = tb.types.ReplyKeyboardMarkup(True, True)
 
-
-def choose_tag(message, avaliable_tags):
+def choose_tag(message, avaliable_tags, selecteg_theme):
     """ создает панель кнопок с тегами """
 
-    tags_keyboard_panel = tb.types.ReplyKeyboardMarkup(True, True)
+    tags_keyboard_panel = tb.types.InlineKeyboardMarkup(row_width=2)
     for tag in avaliable_tags:
-        tags_keyboard_panel.row(tag)
-    bot.send_message(message.chat.id, "По какой теме вы хотите услышать цитату?", reply_markup=tags_keyboard_panel)
+        button = tb.types.InlineKeyboardButton(text=tag, callback_data=tag)
+        tags_keyboard_panel.add(button)
+    bot.send_message(message.chat.id, "Выбери, о чем бы ты хотел услышать {0}".format(selecteg_theme),
+                     reply_markup=tags_keyboard_panel)
+
+
+@bot.callback_query_handler(func=lambda call:True)
+def click_on_tag(call):
+    """ Обработка нажатия inline кнопок """
+
+    bot.send_message(call.message.chat.id, quotes_database['quotes'][randint(0, number_of_quotes)]['text'])
 
 
 @bot.message_handler(commands=['start'])
@@ -60,21 +84,39 @@ def send_greetings(message):
     bot.send_message(message.chat.id, "Приветствую тебя, мой друг", reply_markup=keyboard_commands_root)
 
 
+def send_poem(message):
+    """ пытается отослать стих пользователю """
+
+    try:
+        bot.send_message(message.chat.id, poems_database['poems'][randint(0, number_of_poems)]['text'])
+    except:
+        send_poem(message)
+
+
+def send_biography(message):
+    """ Отсылает пользователю факты из биографии """
+
+    try:
+        bot.send_message(message.chat.id, biography_database['facts'][randint(0, length_of_boigraphy)]['text'])
+    except:
+        send_biography(message)
+
+
 @bot.message_handler(content_types=['text'])
 def send_message(message):
     """ Обрабатывает нажатие кнопок """
 
     if message.text.lower() == "цитата":
-        choose_tag(message, avaliable_statement_tags)
+        try:
+            choose_tag(message, avaliable_quotes_tags, "цитату")
+        except:
+            bot.send_message(message.chat.id, "Попробуйте еще раз")
 
     elif message.text.lower() == "стих":
-        choose_tag(message, avaliable_poem_tags)
+        send_poem(message)
 
     elif message.text.lower() == "биография":
-        bot.send_message(message.chat.id, biography_database['quotes'][randint(0, length_of_boigraphy)]['text'])
-
-    else:
-        bot.send_message(message.chat.id, "что тебе еще надо, друг мой?", reply_markup=keyboard_commands_root)
+        send_biography(message)
 
 
 @bot.message_handler(commands = ['switch'])
@@ -86,4 +128,5 @@ def switch(message):
 
 
 print("Start")
+
 bot.polling()
